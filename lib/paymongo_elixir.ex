5bae06677744d8b2c38416002d3baf5dedd44454
jethro riosa <jethro.riosa@gmail.com>
2020-04-10 22:12:32 +0800

    added documentation

--- lib/paymongo_elixir.ex
+++ lib/paymongo_elixir.ex
@@ -1,9 +1,14 @@
 defmodule PaymongoElixir do
   @moduledoc """
   Documentation for `PaymongoElixir`.
+  for further references visit  [paymongo api references](https://developers.paymongo.com/reference#getting-started-with-your-api)
   """
+
   @url "https://api.paymongo.com"
+  @source "/v1/sources"
   @payment "/v1/payments"
+  @payment_intent "/v1/payment_intents"
+  @payment_method "/v1/payment_methods"
 
   defmacro __using__(opts) do
     otp_app = Keyword.get(opts, :otp_app)
@@ -54,6 +59,164 @@ defmodule PaymongoElixir do
         {:error, :no_payments}
     end
   end
+
+  @doc """
+  get request
+
+  Pass argument request and id
+
+  request can be 
+
+  `:retrieve_payment` retrieves a payment + `id` with format `pay_123`
+  `:retrieve_payment_intent` retrieves a payment intent + `id` with format `pi_123`
+  `:retrieve_payment_method` retrieves a payment method + `id` with format `pm_123`
+
+  if none of the above request was called, an `{:error, :request_not_found}` will be returned.
+
+  ## Example
+  # retrieve a payment
+
+  iex> id = "pay_123"
+  iex> PaymongoElixir.get(:retrieve_payment, id)
+  %{
+    "data" => %{
+      "id" => pay_id,
+      "type" => "payment",
+      "attributes" => %{
+        "status" => "paid"
+        ...
+      }
+    }
+  } 
+
+  ## Example
+  # retrieve a payment intent
+
+  iex> id = "pi_123"
+  iex> PaymongoElixir.get(:retrieve_payment_intent, id)
+  %{
+    "data" => %{
+      "id" => payment_intent_id, 
+      "type" => "payment_intent",
+      "attributes" => %{
+        "amount" => 10000,
+        "currency" => "PHP",
+        "description" => nil,
+        "statement_descriptor" => nil,
+        "status" => "awaiting_payment_method",
+        "livemode" => false,
+        "client_key" => _client_key,
+        "created_at" => _created_at,
+        "updated_at" => _updated_at,
+        "last_payment_error" => nil,
+        "payment_method_allowed" => ["card"],
+        "payment_method_options" => %{
+          "card" => %{"request_three_d_secure" => "automatic"}
+        },
+        "payments" => [],
+        "next_action" => nil,
+        "metadata" => nil
+      }
+    }
+  } 
+
+  ## Example
+  # retrieve a payment method 
+
+  iex> id = "pm_123"
+  iex> PaymongoElixir.get(:retrieve_payment_method, id)
+  %{
+    "data" => %{
+      "id" => payment_method_id,
+      "type" => "payment_method",
+      "attributes" => %{
+        "livemode" => false,
+        "type" => "card",
+        "billing" => nil,
+        "details" => %{
+          "last4" => "4345",
+          "exp_month" => 12,
+          "exp_year" => 2030,
+          "cvc" => "123"
+        }
+      }
+    }
+  }
+
+  """
+  def get(request, id) do
+    with {:ok, %HTTPoison.Response{} = data} <- request(request, id),
+         %{"data" => _data} = data <- Jason.decode!(data.body) do
+      data
+    else
+      %{"errors" => errors} ->
+        {:error, errors}
+
+      {:error, :request_not_found} ->
+        {:error, :request_not_found}
+
+      _ ->
+        {:error, :invalid_transaction}
+    end
+  end
+
+  @doc """
+  post requests  
+  """
+  def post(request, params) do
+    with {:ok, %HTTPoison.Response{} = data} <- request(request, params),
+         %{"data" => _data} = data <- Jason.decode!(data.body) do
+      data
+    else
+      %{"errors" => errors} ->
+        {:error, errors}
+
+      {:error, :request_not_found} ->
+        {:error, :request_not_found}
+
+      _ ->
+        {:error, :invalid_transaction}
+    end
+  end
+
+  # Private Functions
+
+  # request/2
+  defp request(:create_source, params),
+    do: do_request(:post, @source, params, secret_headers())
+
+  defp request(:create_payment_source, params),
+    do: do_request(:post, @payment, params, secret_headers())
+
+  defp request(:retrieve_payment, "pay_" <> _generated_code = id),
+    do: do_request(:get, "#{@payment}/#{id}", secret_headers())
+
+  defp request(:cancel_payment_intent, "pi_" <> _generated_code = id),
+    do: do_request(:post, "#{@payment_intent}/#{id}/cancel", [], secret_headers())
+
+  defp request(
+         :attach_payment_intent,
+         %{"query_params" => query_params, "body_params" => body_params} = _params
+       ) do
+    with client_key <- URI.encode_query(client_key: query_params["client_key"]),
+         query_params <- "#{query_params["id"]}/attach?#{client_key}" do
+      do_request(:post, "#{@payment_intent}/#{query_params}", body_params, secret_headers())
+    end
+  end
+
+  defp request(:retrieve_payment_intent, "pi_" <> _generated_code = id),
+    do: do_request(:get, "#{@payment_intent}/#{id}", secret_headers())
+
+  defp request(:create_payment_intent, params),
+    do: do_request(:post, @payment_intent, params, secret_headers())
+
+  defp request(:retrieve_payment_method, "pm_" <> _generated_code = id),
+    do: do_request(:get, "#{@payment_method}/#{id}", public_headers())
+
+  defp request(:create_payment_method, params),
+    do: do_request(:post, @payment_method, params, public_headers())
+
+  defp request(_request, _params), do: {:error, :request_not_found}
 
   defp request(:list_payments),
     do: do_request(:get, @payment, secret_headers())
